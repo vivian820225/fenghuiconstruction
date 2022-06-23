ac143d1f95512333c4bc5992d54bec432c527b9e
Vivian.C <vivian820225@gmail.com>
2022-06-24 00:01:56 +0800

    [Feat] 網站改版

--- js/main.js
+++ js/main.js
@@ -11,8 +11,16 @@ function onImageLoaded(url, cb) {
 }
 
 function checkImgOnload() {
+  
   const panoramaSection = document.querySelector(".panorama-container");
   let image = new Image();
+  if (innerWidth <= 768) {
+    image.src = "images/s3/m-sec3img01.jpg";
+  } else {
+    image.src = "images/s3/sec3img01.jpg";
+  }
+  // image.src = "images/s3/sec3img01.jpg";
+  console.log(image, image.width);
   image.onload = function () {
     onImageLoaded(`${image.src}`, function () {
       panoramaSection.innerHTML = `<img src="${image.src}" data-width="${image.width}" data-height="${image.height}" alt="Panorama">`;
@@ -22,15 +30,17 @@ function checkImgOnload() {
       bind_resize: true,
     });
   };
-  if (innerWidth <= 768) {
-    image.src = "images/mobile/s4/sec4img01.jpg";
-  } else {
-    image.src = "images/desktop/s4/sec4img01.jpg";
-  }
+
 }
 
 $(document).ready(function () {
-  checkImgOnload();
+  // checkImgOnload();
+
+  $(".panorama-view").panorama360({
+    sliding_controls: false,
+    bind_resize: true,
+    start_position: 400
+  });
 
   // 顯示提示
   // if (window.innerWidth <= 768) {
@@ -40,91 +50,118 @@ $(document).ready(function () {
   //   }, 2 * 1000);
   // }
 
-  const swiper_1 = new Swiper('#swiper_1', {
+  const swiper1 = new Swiper('#swiper-1', {
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
+    
     pagination: {
-      el: ".swiper-pagination",
+      el: "#swiper-1 .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  const swiper_2 = new Swiper('#swiper_2', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  const swiper_3 = new Swiper('#swiper_3', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  const swiper_4 = new Swiper('#swiper_4', {
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  const swiper_5 = new Swiper('#swiper_5', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 48,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '#swiper-1 .swiper-button-next',
+      prevEl: '#swiper-1 .swiper-button-prev',
     },
     breakpoints: {
       768: {
-        slidesPerView: 2,
-        spaceBetween: 48,
+        spaceBetween: 0,
       },
       1024: {
-        slidesPerView: 3,
-        spaceBetween: 48,
+        spaceBetween: 68,
       },
+    },
+  });
+
+  const swiper2 = new Swiper('#swiper-2', {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-2 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#swiper-2 .swiper-button-next',
+      prevEl: '#swiper-2 .swiper-button-prev',
+    },
+  });
+
+  const swiper4 = new Swiper('#swiper-4', {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-4 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#swiper-4 .swiper-button-next',
+      prevEl: '#swiper-4 .swiper-button-prev',
+    },
+  });
+
+  const swiper5 = new Swiper('#swiper-5', {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-5 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#swiper-5 .swiper-button-next',
+      prevEl: '#swiper-5 .swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        spaceBetween: 0,
+      },
+      1024: {
+        spaceBetween: 68,
+      },
+    },
+  });
+
+  const swiper6 = new Swiper('#swiper-6', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-6 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#swiper-6 .swiper-button-next',
+      prevEl: '#swiper-6 .swiper-button-prev',
+    },
+  });
+
+  const swiper7 = new Swiper('#swiper-7', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-7 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#swiper-7 .swiper-button-next',
+      prevEl: '#swiper-7 .swiper-button-prev',
     },
   });
 
